7615fbe67b0b8c0fbe69bb0631e48f5748d60109
EngineX-GB <uk.engine.x@gmail.com>
2026-02-22 17:10:27 +0000

    initial commit

---
+++ phoenix-prototype/WatchlistEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace phoenix_prototype
+{
+    public class WatchlistEntry
+    {
+        [JsonPropertyName("userId")]
+        public string UserId { get; set; }
+       
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("nationality")]
+        public string Nationality { get; set; }
+        
+        [JsonPropertyName("telephone")]
+        public string Telephone { get; set; }
+        
+        [JsonPropertyName("rate")]
+        public int Rate { get; set; }
+        
+        [JsonPropertyName("location")]
+        public string Location { get; set; }
+    }
+}
