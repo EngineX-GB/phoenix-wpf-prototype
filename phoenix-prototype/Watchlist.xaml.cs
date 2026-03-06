0ba77e245472c51f835626533c50f78f559cccb1
EngineX-GB <uk.engine.x@gmail.com>
2026-03-06 14:54:23 +0000

    Removed the thick white border on the watchlist window

--- phoenix-prototype/Watchlist.xaml.cs
+++ phoenix-prototype/Watchlist.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace phoenix_prototype
 {
@@ -13,6 +15,32 @@ namespace phoenix_prototype
     /// </summary>
     public partial class Watchlist : Window
     {
+
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(
+            IntPtr hwnd,
+            int attr,
+            ref int attrValue,
+            int attrSize);
+
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+        private const int DWMWA_BORDER_COLOR = 34;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            var hwnd = new WindowInteropHelper(this).Handle;
+
+            // Enable dark mode frame (prevents white border)
+            int dark = 1;
+            DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, ref dark, sizeof(int));
+
+            // Set border colour (ARGB)
+            int borderColor = unchecked((int)0xFF1A1A1A); // match your window background
+            DwmSetWindowAttribute(hwnd, DWMWA_BORDER_COLOR, ref borderColor, sizeof(int));
+        }
+
         public ObservableCollection<WatchlistEntry> WatchlistCollection { get; set; } = new ObservableCollection<WatchlistEntry>();
         public Watchlist()
         {
@@ -30,7 +58,6 @@ namespace phoenix_prototype
 
             //DataGridWatchlist.ItemsSource = dummyData;
         }
-
 
         public async Task LoadWatchlistAsync()
         {
